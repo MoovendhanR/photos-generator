b353076285ed6f584499a8a312007312c9701f07
MoovendhanR <moovendhanr1011@gmail.com>
2023-02-17 22:19:06 +0530

    intialized with frontend

---
+++ server/src/controllers/Post.controllers.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import {v2 as cloudinary} from 'cloudinary';
+
+
+dotenv.config()
+
+const router = express.Router();
+
+export default router;
--- server/src/index.js
+++ server/src/index.js
@@ -2,19 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
-import connect from "./config/db.js";
 dotenv.config()
- 
-// import postRoutes from "./controllers/Post.controllers.js"
-// import vendhanRoutes from "./controllers/Vendhan.controllers.js"
+
+import connect from "./config/db.js";
+import postRoutes from "./controllers/Post.controllers.js"
+import vendhanRoutes from "./controllers/Vendhan.controllers.js"
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit:"50mb"}));
 
 
-// app.use("/api/v1/post",postRoutes)
-// app.use("/api/v1/vendhan",vendhanRoutes)
+app.use("/api/v1/post",postRoutes)
+app.use("/api/v1/vendhan",vendhanRoutes)
 
 
 const PORT = process.env.port || 5000
