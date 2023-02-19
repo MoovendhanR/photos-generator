2dd36468c4d60d16384d39da6a41819195b60c5f
MoovendhanR <moovendhanr1011@gmail.com>
2023-02-19 22:18:23 +0530

    backend deployed and updated in frontend

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:5000/api/v1/post', {
+      const response = await fetch('https://vendhan-photos-generator.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
